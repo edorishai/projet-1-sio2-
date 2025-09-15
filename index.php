830b770ddb65275cb85299c935c1d10cc256f76d
adrien <adrienlasson2006@gmail.com>
2025-09-15 11:03:53 +0200

    test1

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil - Bibliothèque</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Bienvenue à la Bibliothèque</h1>
+        <a href="page_connexion.php"><button class="btn btn-primary">Connexion</button></a>
+        <a href="inscription.php"><button class="btn btn-success">S'inscrire</button></a>
+    </div>
+</body>
+</html>
